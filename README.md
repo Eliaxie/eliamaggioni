3dbaa1c3515ba61682ffe829aed24c0c1829d252
Elia Maggioni <elia.maggioni@gmail.com>
2022-11-17 22:37:58 +0100

    reworked website

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Website
+https://polinetworkorg.github.io/PoliNetworkAstro/
+
+## Default readme di Astro
+https://github.com/PoliNetworkOrg/PoliNetworkAstro/blob/master/docs/README.md
---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @type-check enabled!
+// VSCode and other TypeScript-enabled text editors will provide auto-completion,
+// helpful tooltips, and warnings if your exported object is invalid.
+// You can disable this by removing "@ts-check" and `@type` comments below.
+
+import WindiCSS from 'vite-plugin-windicss'
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ (
+  {
+    site: 'https://eliaxie.github.io',
+    base: '/eliamaggioni',
+    // Set "renderers" to "[]" to disable all default, builtin component support.
+    // renderers: [],
+    vite: {
+      plugins: [WindiCSS()]
+    }
+  }
+);
